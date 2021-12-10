0ed81581939e52e306f96684671383418f5a8946
rajvimakwanaa <91145653+rajvimakwanaa@users.noreply.github.com>
2021-12-09 19:39:02 -0500

    All changes of Rajvi

---
+++ NGO_System/src/ngo_system/SignUp.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ngo_system;
+
+/**
+ *
+ * @author rajvimakwana
+ */
+public class SignUp {
+    
+    private String username;
+    private String password;
+    private int contact;
+    private String address;
+    private String email;
+    private String ssn;
+    private String dob;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+    
+    
+
+}
+
